f662ace8d153635f8f9e6fea5e4b73365413e6af
DuvCharles <charles.duvigneau@gmail.com>
2022-12-14 15:55:40 +0100

    add logout hook

--- packages/sylius/src/customer/use-customer.tsx
+++ packages/sylius/src/customer/use-customer.tsx
@@ -4,6 +4,7 @@ import useCustomer, {
 } from '@vercel/commerce/customer/use-customer'
 import { getCustomerRoute } from '../utils/token/customer-route'
 import { normalizeCustomer } from '../utils/normalize/normalize-customer'
+import { getCustomerToken } from '../utils/token/customer-token'
 
 export default useCustomer as UseCustomer<typeof handler>
 
@@ -13,12 +14,14 @@ export const handler: SWRHook<any> = {
     method: 'GET',
   },
   fetcher: async ({ options, fetch }) => {
-    const syliusCustomer = await fetch({
-      url: getCustomerRoute() ?? '',
-      method: options.method,
-    })
-    const customer = normalizeCustomer(syliusCustomer)
-    return customer
+    if (getCustomerToken()) {
+      const syliusCustomer = await fetch({
+        url: getCustomerRoute() ?? '',
+        method: options.method,
+      })
+      return normalizeCustomer(syliusCustomer)
+    }
+    return null
   },
   useHook:
     ({ useData }) =>
