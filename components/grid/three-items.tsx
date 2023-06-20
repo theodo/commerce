aab1e1ce5142c5d9565fd07c5c2eb9d59d6bef9d
DuvCharles <charles.duvigneau@gmail.com>
2023-06-20 18:30:59 +0200

    feat: import from lib/sylius

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -1,6 +1,6 @@
 import { GridTileImage } from 'components/grid/tile';
-import { getCollectionProducts } from 'lib/shopify';
-import type { Product } from 'lib/shopify/types';
+import { getCollectionProducts } from 'lib/sylius';
+import type { Product } from 'lib/sylius/types';
 import Link from 'next/link';
 
 function ThreeItemGridItem({
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import FooterMenu from 'components/layout/footer-menu';
 import LogoSquare from 'components/logo-square';
-import { getMenu } from 'lib/shopify';
+import { getMenu } from 'lib/sylius';
 import { Suspense } from 'react';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
@@ -11,7 +11,7 @@ export default async function Footer() {
   const currentYear = new Date().getFullYear();
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
-  const menu = await getMenu('next-js-frontend-footer-menu');
+  const menu = await getMenu();
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
