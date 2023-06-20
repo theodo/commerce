d67c61484cec7692dcf4a37dff40ab0ac14cdaed
DuvCharles <charles.duvigneau@gmail.com>
2023-06-20 18:53:50 +0200

    feat: add types

---
+++ lib/sylius/types.ts
@@ -0,0 +1,81 @@
+export interface Cart {
+  id: string;
+  checkoutUrl: string;
+  cost: {
+    subtotalAmount: Money;
+    totalAmount: Money;
+    totalTaxAmount: Money;
+  };
+  lines: CartItem[];
+  totalQuantity: number;
+}
+
+export interface CartItem {
+  id: string;
+  quantity: number;
+  cost: {
+    totalAmount: Money;
+  };
+  merchandise: {
+    id: string;
+    title: string;
+    selectedOptions: {
+      name: string;
+      value: string;
+    }[];
+    product: Product;
+  };
+}
+
+export interface Money {
+  amount: string;
+  currencyCode: string;
+}
+
+export interface Product {
+  variants: ProductVariant[];
+  images: Image[];
+  id: string;
+  handle: string;
+  availableForSale: boolean;
+  title: string;
+  description: string;
+  descriptionHtml: string;
+  options: ProductOption[];
+  priceRange: {
+    maxVariantPrice: Money;
+    minVariantPrice: Money;
+  };
+  featuredImage: Image;
+  tags: string[];
+  updatedAt: string;
+}
+
+export interface ProductOption {
+  id: string;
+  name: string;
+  values: string[];
+}
+
+export interface ProductVariant {
+  id: string;
+  title: string;
+  availableForSale: boolean;
+  selectedOptions: {
+    name: string;
+    value: string;
+  }[];
+  price: Money;
+}
+
+export interface Image {
+  url: string;
+  altText: string;
+  width: number;
+  height: number;
+}
+
+export interface Menu {
+  title: string;
+  path: string;
+}
