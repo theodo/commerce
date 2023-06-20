3ddcbb747fdbe36c93691a87f577625ce6598c65
DuvCharles <charles.duvigneau@gmail.com>
2023-06-20 18:30:42 +0200

    feat: create sylius folder

--- lib/constants.ts
+++ lib/constants.ts
@@ -25,6 +25,4 @@ export const TAGS = {
   products: 'products'
 };
 
-export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
-export const DEFAULT_OPTION = 'Default Title';
-export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-01/graphql.json';
+export const SYLIUS_API_ENDPOINT = '/api/v2';
