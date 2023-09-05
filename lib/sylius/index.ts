f96a865a45cef573d5104bf2fb9dc4def6e89d66
DuvCharles <charles.duvigneau@gmail.com>
2023-09-05 18:08:17 +0200

    :sparkles: display homePage

--- lib/sylius/index.ts
+++ lib/sylius/index.ts
@@ -1,7 +1,44 @@
 import { SYLIUS_API_ENDPOINT } from 'lib/constants';
 
-const domain = `${process.env.SYLIUS_STORE_DOMAIN}`;
-const endpoint = `${domain}${SYLIUS_API_ENDPOINT}`;
+const DOMAIN = `${process.env.SYLIUS_STORE_DOMAIN}`;
+const ENDPOINT = `${DOMAIN}${SYLIUS_API_ENDPOINT}`;
+
+// Fetch
+export default async function syliusRequest(
+  method: string,
+  path = '',
+  payload?: Record<string, unknown> | undefined
+) {
+  const options: RequestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  if (payload) {
+    options.body = JSON.stringify(payload);
+  }
+
+  try {
+    const result = await fetch(`${ENDPOINT}${path}`, options);
+
+    const body = await result.json();
+
+    if (body.errors) {
+      throw body.errors[0];
+    }
+
+    return {
+      status: result.status,
+      body
+    };
+  } catch (e) {
+    throw {
+      error: e
+    };
+  }
+}
 
 // Pages
 export const getPages = () => [];
@@ -15,11 +52,27 @@ export const getCollection = () => {};
 export const getCollectionProducts = () => [];
 
 // Cart
-export const createCart = () => {};
-export const getCart = () => {};
-export const addToCart = () => {};
+export const createCart = async () => {
+  const cart = await syliusRequest('POST', '/orders', { localeCode: 'fr_FR' });
+  return cart;
+};
+export const getCart = (cartId: string) => {
+  syliusRequest(REST_METHODS.GET, `/orders/${cartId}`);
+  return {};
+};
+export const addToCart = (cartId: string | undefined, payload: AddToCartPayload[]) => {
+  syliusRequest(REST_METHODS.PUT, `/orders/${cartId}/items`, payload[0]);
+  return {};
+};
 export const removeFromCart = () => {};
 export const updateCart = () => {};
 
 // Site
-export const getMenu = () => [];
+export const getMenu = () => [
+  {
+    title: 'Home',
+    path: '/'
+  }
+];
+
+type AddToCartPayload = { merchandiseId: string; quantity: number };
