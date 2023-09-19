e9e9d85aec9c307a1b4b1f0654bb2eb15d8e30e2
DuvCharles <charles.duvigneau@gmail.com>
2023-09-19 19:18:32 +0200

    create routes

--- lib/sylius/index.ts
+++ lib/sylius/index.ts
@@ -1,4 +1,8 @@
 import { REST_METHODS, SYLIUS_API_ENDPOINT } from 'lib/constants';
+import { normalizeCollection } from './normalizer/collection-normalizer';
+import { normalizeProduct } from './normalizer/product-normalizer';
+import { SyliusProduct, SyliusTaxon } from './sylius-types/product-types';
+import { AddToCartPayload, GetCollectionProductsPayload, GetProductsPayload } from './types';
 
 const DOMAIN = `${process.env.SYLIUS_STORE_DOMAIN}`;
 const ENDPOINT = `${DOMAIN}${SYLIUS_API_ENDPOINT}`;
@@ -12,7 +16,8 @@ export default async function syliusRequest(
   const options: RequestInit = {
     method,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
     }
   };
 
@@ -45,11 +50,100 @@ export const getPages = () => [];
 export const getPage = () => {};
 
 // Products
-export const getProducts = () => [];
-export const getProduct = () => {};
-export const getProductRecommendations = () => [];
-export const getCollection = () => {};
-export const getCollectionProducts = () => [];
+export const getProducts = async (payload: GetProductsPayload) => {
+  const url = new URL(`${ENDPOINT}/products`);
+  if (payload.query) {
+    url.searchParams.set('translations.name', payload.query);
+  }
+  const orderBy = payload.reverse ? 'desc' : 'asc';
+
+  if (payload.sortKey) {
+    switch (payload.sortKey) {
+      case 'RELEVANCE':
+        break;
+      case 'BEST_SELLING':
+        break;
+      case 'CREATED_AT':
+        url.searchParams.set('order[createdAt]', orderBy);
+        break;
+      case 'PRICE':
+        url.searchParams.set('order[price]', orderBy);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const data = await syliusRequest(REST_METHODS.GET, '/products' + url.search);
+  const syliusProducts = data.body;
+  const products = syliusProducts.map((syliusProduct: SyliusProduct) =>
+    normalizeProduct(syliusProduct)
+  );
+  return products;
+};
+
+export const getProduct = async (slug: string) => {
+  const data = await syliusRequest(REST_METHODS.GET, '/products-by-slug/' + slug);
+
+  const syliusProduct = data.body;
+  const product = normalizeProduct(syliusProduct);
+
+  return product;
+};
+export const getProductRecommendations = () => {
+  return [];
+};
+export const getCollections = async () => {
+  const data = await syliusRequest(REST_METHODS.GET, '/taxons');
+
+  const syliusTaxons = data.body;
+  const collections = syliusTaxons.map((syliusTaxon: SyliusTaxon) =>
+    normalizeCollection(syliusTaxon)
+  );
+
+  return collections;
+};
+
+export const getCollection = async (taxonCode: string) => {
+  const data = await syliusRequest(REST_METHODS.GET, '/taxons/' + taxonCode);
+
+  const syliusTaxon = data.body;
+  const collection = normalizeCollection(syliusTaxon);
+
+  return collection;
+};
+
+export const getCollectionProducts = async (payload: GetCollectionProductsPayload) => {
+  const url = new URL(`${ENDPOINT}/products`);
+  if (payload.collection) {
+    url.searchParams.set('productTaxons.taxon.code', payload.collection);
+  }
+  const orderBy = payload.reverse ? 'desc' : 'asc';
+
+  if (payload.sortKey) {
+    switch (payload.sortKey) {
+      case 'RELEVANCE':
+        break;
+      case 'BEST_SELLING':
+        break;
+      case 'CREATED_AT':
+        url.searchParams.set('order[createdAt]', orderBy);
+        break;
+      case 'PRICE':
+        url.searchParams.set('order[price]', orderBy);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const data = await syliusRequest(REST_METHODS.GET, '/products' + url.search);
+  const syliusProducts = data.body;
+  const products = syliusProducts.map((syliusProduct: SyliusProduct) =>
+    normalizeProduct(syliusProduct)
+  );
+  return products;
+};
 
 // Cart
 export const createCart = async () => {
@@ -70,9 +164,7 @@ export const updateCart = () => {};
 // Site
 export const getMenu = () => [
   {
-    title: 'Home',
-    path: '/'
+    title: 'All',
+    path: '/search'
   }
 ];
-
-type AddToCartPayload = { merchandiseId: string; quantity: number };
